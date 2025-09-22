1429312fd7a9b86302d886e7816b27a3a1d48e9a
UmerAnsari222 <umerfaqrooq78@gmail.com>
2025-09-22 17:23:26 +0500

    Feat: Try to fix actions 1

--- src/app.ts
+++ src/app.ts
@@ -26,11 +26,11 @@ app.set("views", __dirname + "/views");
 
 console.log(new Date("2025-09-18T23:59:59.000Z"));
 
-// app.get("/", (req, res) => {
-//   return res
-//     .status(200)
-//     .json({ msg: "Welcome to DigitalDojo API", success: true });
-// });
+app.get("/", (req, res) => {
+  return res
+    .status(200)
+    .json({ msg: "Welcome to DigitalDojo API", success: true });
+});
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/rest-password", forgotPasswordRouter);
