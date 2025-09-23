c803855da81a58b5b59e6f08e71de1286137edd6
UmerAnsari222 <umerfaqrooq78@gmail.com>
2025-09-23 12:48:13 +0500

    Adjustment: Some changes in / route

--- src/app.ts
+++ src/app.ts
@@ -29,7 +29,7 @@ console.log(new Date("2025-09-18T23:59:59.000Z"));
 app.get("/", (req, res) => {
   return res
     .status(200)
-    .json({ msg: "Welcome to DigitalDojo API 2", success: true });
+    .json({ msg: "Welcome to DigitalDojo API", success: true });
 });
 
 app.use("/api/v1/auth", authRouter);
