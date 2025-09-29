64cbee455f1ed803f02ad7b46923b61d9b09a85e
UmerAnsari222 <umerfaqrooq78@gmail.com>
2025-09-29 13:26:50 +0500

    Adjustemnt: For Logger

--- dist/middlewares/error.js
+++ dist/middlewares/error.js
@@ -1,7 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+const logger_1 = __importDefault(require("../config/logger"));
 // Error-handling middleware
 const errorMiddleware = (err, req, res, next) => {
+    logger_1.default.error(err); // ✅ logs full stack trace if logger is set up correctly
     const status = err.statusCode || 500;
     const message = err.message || "Internal Server Error";
     res.status(status).json({
--- src/middlewares/error.ts
+++ src/middlewares/error.ts
@@ -1,5 +1,6 @@
 import { ErrorRequestHandler, NextFunction, Request, Response } from "express";
 import ErrorHandler from "../utils/error";
+import logger from "../config/logger";
 
 // Error-handling middleware
 const errorMiddleware: ErrorRequestHandler = (
@@ -8,6 +9,7 @@ const errorMiddleware: ErrorRequestHandler = (
   res: Response,
   next: NextFunction
 ) => {
+  logger.error(err); // ✅ logs full stack trace if logger is set up correctly
   const status = err.statusCode || 500;
   const message = err.message || "Internal Server Error";
 
